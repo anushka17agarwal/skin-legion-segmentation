18c339f3a9b4303f8622c4afeb19021aeb98a1cd
Anushka agarwal <40672880+anushka17agarwal@users.noreply.github.com>
2022-01-01 11:08:48 +0530

    Update data.py

--- data.py
+++ data.py
@@ -25,15 +25,37 @@ class SkinData(Dataset):
     def __getitem__(self, index):
         #print(self.image)
         img_path= os.path.join(self.image_dir, self.image[index])
+        
         image = np.array(Image.open(img_path).convert("RGB"))
         image= image.astype(float)
-        mask_path = os.path.join(self.mask_dir, self.mask[index])
+        mask_path = os.path.join(self.mask_dir, self.image[index].replace(".jpg", "_segmentation.png"))
+        #print(index, img_path, mask_path)
         mask = np.array(Image.open(mask_path).convert("L"), dtype=np.float32)
         mask[mask == 255.0] = 1.0
-
+        #print("img path", img_path, "mask path",mask_path)
         if self.transform is not None:
             augmentations = self.transform(image=image, mask=mask)
             image = augmentations["image"]
             mask = augmentations["mask"]
         return image, mask
        
+# test_path= "test_data"
+# val_transforms = A.Compose(
+#     [
+#         A.Resize(height=150, width=150),
+#         A.Normalize(
+#             mean=[0.0, 0.0, 0.0],
+#             std=[1.0, 1.0, 1.0],
+#             max_pixel_value=255.0,
+#         ),
+#         ToTensorV2(),
+#     ],
+# )
+# test_dataset= SkinData(test_path, transform= val_transforms)
+
+
+# test_loader = DataLoader(test_dataset, batch_size= 3, shuffle=True)
+# dataiter= iter(test_loader)
+# images, labels = dataiter.next()
+
+
